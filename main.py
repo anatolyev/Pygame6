a126095177354f07eb63c0a6326f9e4c3ea1f06d
anatolyev <anatolyev@yandexlyceum.ru>
2022-12-15 17:43:23 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,95 @@
+import pygame
+import os
+import sys
+import random
+
+pygame.init()
+SIZE = WIDTH, HEIGHT = 400, 400
+SCREEN = pygame.display.set_mode(SIZE)
+
+
+
+class Ball(pygame.sprite.Sprite):
+    def __init__(self, radius, x, y):
+        super().__init__(all_sprites)
+        self.radius = radius
+        self.image = pygame.Surface((2 * radius, 2 * radius), pygame.SRCALPHA, 32)
+        pygame.draw.circle(self.image, pygame.Color("red"), (radius, radius), radius)
+        self.rect = pygame.Rect(x, y, 2*radius, 2*radius)
+        self.vx = random.randint(-5, 5)
+        self.vy = random.randrange(-5, 5)
+    def update(self):
+        self.rect = self.rect.move(self.vx, self.vy)
+        if pygame.sprite.spritecollideany(self, horizontal_borders):
+            self.vy = -self.vy
+        if pygame.sprite.spritecollideany(self, vertical_borders):
+            self.vx = -self.vx
+
+
+class Border(pygame.sprite.Sprite):
+    def __init__(self, x1, y1, x2, y2):
+        super().__init__(all_sprites)
+        if x1 == x2:
+            # вертикальная стенка
+            self.add(vertical_borders)
+            self.image = pygame.Surface([1, y2 - y1])
+            self.rect = pygame.Rect(x1, y1, 1, y2 - y1)
+        else:
+            # горизонтальная стенка
+            self.add(horizontal_borders)
+            self.image = pygame.Surface([x2 - x1, 1])
+            self.rect = pygame.Rect(x1, y1, x2 - x1, 1)
+
+
+
+
+def load_image(name, colorkey=None):
+    fullname = os.path.join('images', name)
+    if not os.path.isfile(fullname):
+        print(f'Файл с изображением {fullname} не найден')
+        sys.exit()
+    image = pygame.image.load(fullname)
+    if colorkey is not None:
+        image = image.convert()
+        if colorkey == -1:
+            colorkey = image.get_at((0, 0))
+        image.set_colorkey(colorkey)
+    else:
+        image = image.convert_alpha()
+    return image
+
+
+# def main():
+clock = pygame.time.Clock()
+pygame.display.set_caption('шарики')
+horizontal_borders = pygame.sprite.Group()
+vertical_borders = pygame.sprite.Group()
+all_sprites = pygame.sprite.Group()
+Border(5, 5, WIDTH - 5, 5)
+Border(5, HEIGHT - 5, WIDTH - 5, HEIGHT - 5)
+Border(5, 5, 5, HEIGHT - 5)
+Border(WIDTH - 5, 5, WIDTH - 5, HEIGHT - 5)
+for i in range(10):
+    Ball(20, 200, 200)
+running = True
+while running:
+    # внутри игрового цикла ещё один цикл
+    # приёма и обработки сообщений
+    for event in pygame.event.get():
+        # при закрытии окна
+        if event.type == pygame.QUIT:
+            running = False
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            all_sprites.update(event)
+    # отрисовка и изменение свойств объектов
+
+    SCREEN.fill("white")
+    all_sprites.draw(SCREEN)
+    all_sprites.update()
+    clock.tick(30)  # 30 кадров в секунду
+    pygame.display.flip()
+pygame.quit()
+
+
+# if __name__ == '__main__':
+#     main()
